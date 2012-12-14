ac90d03d6a57ab7907ecd33d1390e4ff8ef7ee25
Damien Wilson <damien@mindglob.com>
2012-12-14 05:19:06 -0800

    Clean up bootstrap a bit

--- bootstrap.lua
+++ bootstrap.lua
@@ -1,1 +1,7 @@
-print("Congratulations! You've started installing cc-scripts!")
+-- Name: bootstrap
+-- Source: /cc-scripts/bootstrap.lua
+-- The cc-scripts installer.
+
+term.clear()
+textutils.slowPrint("cc-scripts installer has been initialized!")
+
