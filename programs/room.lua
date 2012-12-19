2ef0a0f36bfd4ea409fd5547edf32949dc3cc28b
Damien Wilson <damien@mindglob.com>
2012-12-18 22:07:30 -0800

    WIP: Re-implement `room` script

--- programs/room.lua
+++ programs/room.lua
@@ -6,113 +6,152 @@ if #tArgs ~= 3 then
   return
 end
 
-local length = tonumber( tArgs[1] )
-local width  = tonumber( tArgs[2] )
-local height = tonumber( tArgs[3] )
+local nLength = tonumber( tArgs[1] )
+local nWidth  = tonumber( tArgs[2] )
+local nHeight = tonumber( tArgs[3] )
 
-function dig()
-  while turtle.detect() do
-    turtle.dig()
+-- Detects the presence of a block in the provided direction.
+--
+-- Valid directions are "forward", "down", or "up".
+-- Providing no direction will default to the direction "forward".
+-- Providing an invalid or mistyped direction will raise an error.
+--
+-- Returns a boolean (true or false).
+--
+-- Examples:
+--
+--   detect("up")      --> true
+--   detect()          --> true
+--   detect("invalid") --> error
+function detect(direction)
+  local tDetectDirections = {
+    forward = turtle.detectUp,
+    down    = turtle.detectDown,
+    up      = turtle.detectUp
+  }
+
+  if type(direction) == "nil" then
+    -- default to detecting the block in front of us
+    fDetect = tDetectDirections["forward"]
+  elseif type(tDetectDirections[direction]) == "nil" then
+    -- raise an error when provided an invalid direction
+    error("Invalid direction passed to detect(), valid directions are 'forward', 'up', or 'down'")
+  else
+    fDetect = tDetectDirections[direction]
+  end
+
+  return fDetect()
+end
+
+-- Digs a number of blocks in the provided direction.
+--
+-- Valid directions are "forward", "down", or "up".
+-- Providing no direction will default to the direction "forward".
+-- Providing an invalid or mistyped direction will raise an error.
+--
+-- Paramaters:
+--
+--  * distance (number, optional, default: 1)
+--    The number of blocks to dig
+--
+--  * direction (string, optional, default: "forward")
+--    The direction in which to dig. If provided, must be
+--    one of "forward", "up", or "down". 
+--
+-- Returns a boolean (true or false).
+--
+-- Examples:
+--
+--   dig()          --> dig forward one block
+--   dig(3)         --> dig forward three blocks
+--   dig("up")      --> dig up one block
+--   dig("down", 3) --> dig down three blocks
+--   dig(5, "up")   --> dig up five blocks
+function dig(...)
+  local tArgs = {...}
+
+  if #tArgs > 2 then
+    error("Too many arguments passed to dig() (min 0, max 2)")
+  end
+
+  local tDigDirections = {
+    forward = turtle.dig,
+    down    = turtle.digDown,
+    up      = turtle.digUp
+  }
+
+  -- Determine if we were provided a distance,
+  -- a direction, or both. Additionally,
+  -- figure out what order they were provided in.
+  local nDistance  = 1         -- default to 1 block
+  local sDirection = "forward" -- default to forward
+
+  -- detect distance
+  if type(tArgs[1]) == "number" then
+    nDistance = tArgs[1]
+  else
+    nDistance = tArgs[2] or nDistance
+  end
+
+  -- detect direction
+  if type(tArgs[1]) == "string" then
+    sDirection = tArgs[1]
+  else
+    sDirection = tArgs[2] or sDirection
+  end
+
+  -- Ensure we have a valid distance
+  if not nDistance > 0 then
+    error("Invalid distance passed to dig(), distance must be a number greater than 0")
+  end
+
+  -- Ensure we have a valid direction
+  if type(tDigDirections[sDirection]) ~= "function" then
+    error("Invalid direction passed to dig(), valid directions are 'forward', 'up', or 'down'")
+  end
+
+  -- Pull the out dig function we want and
+  -- reference it with fDig()
+  local fDig = tDigDirections[sDirection]
+
+  local bSuccess = false
+
+  for _ = 1, nDistance do
+    -- if there is a block in direction we're going
+    if detect(direction) then
+      -- attempt to break the block
+      bSuccess = fDig()
+    end
+
+    -- We ran into something we can't dig, possibly
+    -- bedrock, a protected block, or a mod block.
+    if bSuccess == false then
+      error("Encountered an un-diggable block")
+    end
+
+    -- Wait a bit to see if digging caused blocks
+    -- around the block we dug to move around.
+    -- This can happen if we're dealing with sand,
+    -- gravel, or anvils.
     sleep(0.5)
-  end
-end
 
-function digUp()
-  while turtle.detectUp() do
-    turtle.digUp()
-    sleep(0.5)
-  end
-end
+    -- If something fell in our way...
+    if detect(direction) then
+      -- keep digging until the way is clear.
+      while detect(direction) do
+        -- Account for un-diggable blocks
+        if not dig(direction) then
+          error("Encountered an un-diggable block")
+        end
 
-function forward()
-  if not turtle.forward() then
-    dig()
-    turtle.forward()
-  end
-end
-
-function up()
-  if not turtle.up() then
-    digUp()
-    turtle.up()
-  end
-end
-
-function digRow(rowLength, digAbove)
-  digAbove = digAbove or false
-
-  for i = 1, rowLength do
-    dig()
-    forward()
-    if digAbove then digUp() end
-  end
-end
-
-function digLevel(desiredHeight, currentHeight)
-  for i = 1, width do
-    -- dig the full length on the first row of the first level
-    if i == 1 and currentHeight == 1 then
-      -- dig out addiontal level if needed
-      if currentHeight == desiredHeight then
-        digRow(length)
-      else
-        digRow(length, true)
+        -- Give blocks time to fall before we continue
+        sleep(0.5)
       end
 
-    -- skip the first block of each lengthwise column on
-    -- all subsequent columns; in these instances the
-    -- turtle is already positioned at the first block
-    -- of the given column.
-    else
-      -- Position the turtle in the next column,
-      -- moving it in a zig-zag pattern as we dig
-      -- out the full width of the room
-      -- 
-      -- If we're digging out an even number of floors,
-      -- figure out if half of the current height is even;
-      -- otherwise we only care if the current height is even.
-      local even = false
-
-      if desiredHeight % 2 == 0 then
-        even = ((desiredHeight / 2) % 2 == 0)
-      else
-        even = (i % 2 == 0)
-      end
-
-      if even then
-        turtle.turnRight()
-        forward()
-        turtle.turnRight()
-      else
-        turtle.turnLeft()
-        forward()
-        turtle.turnLeft()
-      end
-
-      -- Dig out an addional level if possible
-      if (desiredHeight - currentHeight) == 0 then
-        digRow(length - 1)
-      else
-        digRow(length - 1, true)
-      end
-    end -- length
-  end -- width
-end
-
-for j = 1, height do
-  digLevel(height, j)
-
-  -- Position turtle to dig out next level,
-  -- but only if there are more levels to dig
-  if (height - j) ~= 0 then
-    turtle.turnRight()
-    turtle.turnRight()
-    up()
-
-    -- Go up two levels if we
-    -- haven't hit the ceiling
-    if height - j - 1 ~= 0 then up() end
+      -- Nothing there, we have succeeded!
+      bSuccess = true
+    end
   end
-end -- height
 
-print("Done!")
+  return bSuccess
+end
