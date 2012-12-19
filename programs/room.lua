987ecc418dfb40e6cacb10f57f86d23c4e7dc9d1
Damien Wilson <damien@mindglob.com>
2012-12-18 20:03:48 -0800

    Improvements to `room` script
    Better handle change in elevation when digging out rooms for rooms with
    both odd and even numbered heights.

--- programs/room.lua
+++ programs/room.lua
@@ -42,14 +42,14 @@ function digRow(rowLength, digAbove)
 end
 
 function digLevel(desiredHeight, currentHeight)
-   for i = 1, width do
+  for i = 1, width do
     -- dig the full length on the first row of the first level
     if i == 1 and currentHeight == 1 then
       -- dig out addiontal level if needed
-      if currentHeight < desiredHeight then
-        digRow(length, true)
-      else
+      if currentHeight == desiredHeight then
         digRow(length)
+      else
+        digRow(length, true)
       end
       
     -- skip the first block of each lengthwise column on
@@ -60,41 +60,50 @@ function digLevel(desiredHeight, currentHeight)
       -- Position the turtle in the next column,
       -- moving it in a zig-zag pattern as we dig
       -- out the full width of the room
+      if i == 1 then
+        -- If we're digging out an even number of floors,
+        -- figure out if half of the current height is even;
+        -- otherwise we only care if the current height is even.
+        local even = false
 
-      -- If we're digging out an even number of floors,
-      -- figure out if half of the current height is even;
-      -- otherwise we only care if the current height is even.
-      local even = false
-      if desiredHeight % 2 == 0 then
-        even = ((desiredHeight / 2) % 2 == 0)
-      else
-        even = (i % 2 == 0)
-      end
+        if desiredHeight % 2 == 0 then
+          even = ((desiredHeight / 2) % 2 == 0)
+        else
+          even = (i % 2 == 0)
+        end
 
-      if even then
-        turtle.turnRight()
-        forward()
-        turtle.turnRight()
-      else
-        turtle.turnLeft()
-        forward()
-        turtle.turnLeft()
+        if even then
+          turtle.turnRight()
+          forward()
+          turtle.turnRight()
+        else
+          turtle.turnLeft()
+          forward()
+          turtle.turnLeft()
+        end
       end
       
       -- dig out addional level if possible
-      if currentHeight < desiredHeight then
-        digRow(length - 1, true)
-      else
+      if (desiredHeight - currentHeight) == 0 then
         digRow(length - 1)
+      else
+        digRow(length - 1, true)
       end
     end -- length
   end -- width
 end
 
 for j = 1, height do
-  -- For even heights we dig out floors two at a time,
-  -- so skip digging on odd numbered heights
-  if height % 2 == 0 then
-    if j % 2 == 0 then digLevel(height, j) end
-  end
+  digLevel(height, j)
+
+  -- Position turtle to dig out next level
+  turtle.turnRight()
+  turtle.turnRight()
+  up()
+
+  -- Go up two levels if we
+  -- haven't hit the ceiling
+  if height - j ~= 0 then up() end
 end -- height
+
+print("Done!")
